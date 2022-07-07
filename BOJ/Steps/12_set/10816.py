47d4012cbc29d5df2905f7b57bf04533aa18028d
DK <104574852+nanunsaram@users.noreply.github.com>
2022-07-07 18:28:07 +0200

    Update 10816.py

--- BOJ/Steps/12_set/10816.py
+++ BOJ/Steps/12_set/10816.py
@@ -1,18 +1,29 @@
 import sys
 
 N = int(input())
-
-# Make a list for counting the appearance of each number in hands
-# Given hands are greater than or equal to -10,000,000 and less than or equal to 10,000,000
 hands = list(map(int, sys.stdin.readline().split()))
-hands_cnt = [0]*(2*(10**7)+1)
-for i in range(N):
-    hands_cnt[hands[i] + 10**7] += 1
+
+hands_count = {}
+for hand in hands:
+    if hand in hands_count:
+        hands_count[hand] += 1
+    else:
+        hands_count[hand] = 1
 
 M = int(input())
 query = list(map(int, sys.stdin.readline().split()))
 
-# For each queried number, print the element from the list for counting
-for i in range(M):
-    print(hands_cnt[query[i]+10**7], end=' ')
+# I did some 'unexpected' experiments while trying to solve this problem
+# Timeout displays if I use
+#     if each_query in hands:
+# instead of
+#     if each_query in hands_count:
+# It is meaningless to check the original list if we already have (supposedly) more concise dictionary
+# For huge amounts of data, it could be critical
+
+for each_query in query:
+    if each_query in hands_count:
+        print(hands_count[each_query], end=' ')
+    else:
+        print(0, end=' ')
 print()
