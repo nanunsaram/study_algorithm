79cb57f44d8bcb1dcac9e21e68136ab67385c9ff
DK <104574852+nanunsaram@users.noreply.github.com>
2022-07-08 11:53:01 +0200

    Update 9012.py

--- BOJ/Algorithm_Basic/200/9012.py
+++ BOJ/Algorithm_Basic/200/9012.py
@@ -1,16 +1,24 @@
+# https://wookcode.tistory.com/49 참고하였습니다.
+
+# This problem is intended for stack
+# It is also possible to solve by counting the parenthesis(left +1, right -1)
+
 def VPS_checker(s):
-    parenthese_cnt = 0
+    parenthese_stack = []
     for parenthese in s:
         if parenthese == "(":
-            parenthese_cnt += 1
+            parenthese_stack.append(parenthese)
         else:
-            parenthese_cnt -= 1
-            if parenthese_cnt < 0:
+            if parenthese_stack:
+                parenthese_stack.pop()
+            else:
+                print("NO")
                 break
-    if parenthese_cnt == 0:
-        print("YES")
     else:
-        print("NO")
+        if not parenthese_stack:
+            print("YES")
+        else:
+            print("NO")
 
 N = int(input())
 for i in range(N):
